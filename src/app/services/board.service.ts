c3b7ffe8985423ba28deea77b0ea6f21bc8b9e4c
herrickz <herrickz1120@gmail.com>
2018-12-12 08:06:07 -0500

    Implemented board component as an async observable that shows loading when fetching from service

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { SimpleBoard } from '../models/simple-board';
 import { Board } from '../models/board';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,4 +22,5 @@ export class BoardService {
   getBoard(hashId: string): Observable<Board> {
     return this.http.get<Board>(`${environment.apiUrl}/api/board/${hashId}/`)
   }
+  
 }

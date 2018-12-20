c7eccb550885ab5757d5f2d11a09504f9fca8a2b
herrickz <herrickz1120@gmail.com>
2018-12-20 12:29:30 -0500

    changed name of httpMock to mockHttp

--- src/app/services/board.service.unit.spec.ts
+++ src/app/services/board.service.unit.spec.ts
@@ -5,32 +5,32 @@ import { BoardService } from './board.service';
 describe('BoardService Unit', () => {
 
   let boardService: BoardService
-  let httpMock;
+  let mockHttp;
 
   beforeEach(() => {
-    httpMock = jasmine.createSpyObj('http', ['get', 'post']);
+    mockHttp = jasmine.createSpyObj('http', ['get', 'post']);
 
-    boardService = new BoardService(httpMock);
+    boardService = new BoardService(mockHttp);
   });
 
   it('getBoards should call http.get with http://localhost:8000/api/board/', () => {
 
     boardService.getBoards();
 
-    expect(httpMock.get).toHaveBeenCalledWith('http://localhost:8000/api/board/');
+    expect(mockHttp.get).toHaveBeenCalledWith('http://localhost:8000/api/board/');
   });
 
   it('getBoard with abc123 should call http.get with http://localhost:8000/api/board/abc123/', () => {
 
     boardService.getBoard('abc123');
 
-    expect(httpMock.get).toHaveBeenCalledWith('http://localhost:8000/api/board/abc123/')
+    expect(mockHttp.get).toHaveBeenCalledWith('http://localhost:8000/api/board/abc123/')
   });
 
   it('createBoard with name test board should call http.post with http://localhost:8000/api/board/ and { name: \'test board\' }', () => {
 
     boardService.createBoard('test board');
 
-    expect(httpMock.post).toHaveBeenCalledWith('http://localhost:8000/api/board/', { name: 'test board' });
+    expect(mockHttp.post).toHaveBeenCalledWith('http://localhost:8000/api/board/', { name: 'test board' });
   });
 });
